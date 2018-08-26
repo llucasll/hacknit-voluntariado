f18c304200faa5a40267b61c3a3eccf61ba2b91f
JennifferCrystine <jennifercrystine@id.uff.br>
2018-08-26 00:29:43 -0300

    adimnbhu

--- grupo9/src/pages/projeto.php
+++ grupo9/src/pages/projeto.php
@@ -10,8 +10,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="src/css/projeto.css">
-		<script src="src/js/projeto.js"></script>
+		<link rel="stylesheet" type="text/css" href="../src/css/projeto.css">
+		<script src="../src/js/projeto.js"></script>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 	</head>
 	<body>
